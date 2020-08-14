84a123623f6c30736bd20cdfbe1c81348772c70d
sai-myne <ahlusir@gmail.com>
2020-08-14 16:12:15 +0800

    Crating a Modal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
 import ImageGrid from './comps/imageGrid';
+import Modal from './comps/Modal';
+import { useState } from 'react';
 
 function App() {
+
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title/>
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
     </div>
   );
 }
